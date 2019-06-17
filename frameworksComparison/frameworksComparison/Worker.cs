96e2dfa31b72b78a64ba3634cdc6e7ece3bbe5b0
lil-dead <andrijuganov@gmail.com>
2019-06-18 02:19:53 +0300

    Added project

---
+++ frameworksComparison/frameworksComparison/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace frameworksComparison
+{
+    public class Worker
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public int SpecialityId { get; set; }
+        public int BossId { get; set; }
+        public bool IsBoss { get; set; }
+    }
+}
